fb327be690e199cc26f7791e7395b81054755b6d
andreanot <116190697+andreanot@users.noreply.github.com>
2022-12-09 23:10:42 +0100

    Homework 1

---
+++ app.js
@@ -0,0 +1,9 @@
+let unitPrice = 119.95;
+let taxRate = 0.05;
+
+let totalPrice = unitPrice * 30;
+let totalTax = totalPrice * taxRate;
+
+let finalPrice = totalPrice + totalTax;
+
+console.log(`The final price for 30 phones is $${finalPrice.toFixed(2)}.`);
