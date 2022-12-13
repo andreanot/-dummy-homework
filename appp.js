d6517d3ae2818a1f463a85c80416853a0d17f4a7
andreanot <116190697+andreanot@users.noreply.github.com>
2022-12-13 16:14:03 +0100

    Add files via upload

---
+++ appp.js
@@ -0,0 +1,38 @@
+let year = prompt('Enter a year to find out your Chinese Zodiac sign');
+let Zodiac = (year - 4) % 12;
+if ( Zodiac = 0 ) {
+    console.log ('This year is in the Rat zodiac');
+}
+  else ( Zodiac = 1 ) {
+    console.log ('This year is in the Ox zodiac');
+}
+  else ( Zodiac = 2 ) {
+    console.log ('This year is in the Tiger zodiac');
+}
+  else ( Zodiac = 3 ) {
+    console.log ('This year is in the Rabbit zodiac');
+}
+  else ( Zodiac = 4 ) {
+    console.log ('This year is in the Dragon zodiac');
+}
+  else ( Zodiac = 5 ) {
+    console.log ('This year is in the Snake zodiac');
+}
+  else ( Zodiac = 6 ) {
+    console.log ('This year is in the Horse zodiac');
+}
+  else ( Zodiac = 7 ) {
+    console.log ('This year is in the Goat zodiac');
+}
+  else ( Zodiac = 8 ) {
+    console.log ('This year is in the Monkey zodiac');
+}
+  else ( Zodiac = 9 ) {
+    console.log ('This year is in the Rooster zodiac');
+}
+  else ( Zodiac = 10 ) {
+    console.log ('This year is in the Dog zodiac');
+}
+  else ( Zodiac = 11 ) {
+    console.log ('This year is in the Pig zodiac');
+}
